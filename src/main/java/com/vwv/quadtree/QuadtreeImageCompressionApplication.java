4d72a9c6c699139d878cc77a800813b9d989096d
flow6979 <vaibhav.dixit@practo.com>
2025-03-23 16:08:45 +0530

    added entry point of application

---
+++ src/main/java/com/vwv/quadtree/QuadtreeImageCompressionApplication.java
@@ -0,0 +1,13 @@
+package com.vwv.quadtree;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadtreeImageCompressionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadtreeImageCompressionApplication.class, args);
+    }
+}
